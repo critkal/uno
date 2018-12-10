7020abe842cccf3d417b3f399f242e41a5067e6e
João Victor <victorbarboza@outlook.com>
2018-12-10 18:25:42 -0300

    Corrections

--- src/CardModel/ActionCard.java
+++ src/CardModel/ActionCard.java
@@ -13,7 +13,7 @@ public class ActionCard extends UNOCard{
 	}
 	
 	/*@ requires cardColor != null && cardValue != null;
-	  @ requires cardColor == Interfaces.UNOConstants.BLUE;
+	  @ requires cardColor == Interfaces.UNOConstants.BLUE || cardColor == Interfaces.UNOConstants.RED || cardColor == Interfaces.UNOConstants.YELLOW || cardColor == Interfaces.UNOConstants.GREEN;
 	  @ requires cardValue.equals(Interfaces.UNOConstants.DRAW2PLUS) || cardValue.equals(Interfaces.UNOConstants.SKIP) || cardValue.equals(Interfaces.UNOConstants.REVERSE);
 	  @ assignable this.cardColor, this.type, this.value;
 	  @ ensures this.cardColor == cardColor;
